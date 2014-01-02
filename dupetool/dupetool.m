1242f20a8d35270e5482922f4b58bf2bb7bcc5be
Jonathan Fischer <jonathan@mohiji.org>
2014-01-02 12:41:06 -0800

    Project created.

---
+++ dupetool/dupetool.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  dupetool
+//
+//  Created by Jonathan Fischer on 1/2/14.
+//  Copyright (c) 2014 Jonathan Fischer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
